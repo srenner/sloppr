4a5f1a50b5af1f2d604d320adc768be53c600d33
Shawn Renner <shawn.renner@gmail.com>
2026-07-20 12:20:57 -0500

    initial models

---
+++ sloppr/Models/KeyIngredient.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace sloppr.Models;
+
+/// <summary>
+/// An ingredient the user specifically requested for use
+/// </summary>
+public class KeyIngredient : BaseModel
+{
+    public string Name { get; set; }
+    public int NumQueried { get; set; }
+    public int NumUsed { get; set; }
+    public DateTime? LastUsed { get; set; }
+}
+
---
+++ sloppr/Models/Recipe.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace sloppr.Models;
+
+/// <summary>
+/// Full meal recipe
+/// </summary>
+public class Recipe : BaseModel
+{
+    public required string FullText { get; set; }
+    public string TextFormat { get; set; } = "md";
+    public int PrepTime { get; set; } = 0;
+    public int CookTime { get; set; } = 0;
+
+}
