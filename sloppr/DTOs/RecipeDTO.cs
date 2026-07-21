395ab3c63882c0945969c3641de67ade12c4f73e
Shawn Renner <shawn.renner@gmail.com>
2026-07-20 20:47:47 -0500

    dto prompt refinement, add dtos

---
+++ sloppr/DTOs/RecipeDTO.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace sloppr.DTOs;
+
+public class RecipeDTO
+{
+    public int Id { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime DateUpdated { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsDeleted { get; set; }
+    public string CreatedBy { get; set; }
+    public string UpdatedBy { get; set; }
+
+    public required string FullText { get; set; }
+    public string TextFormat { get; set; } = "md";
+    public int PrepTime { get; set; } = 0;
+    public int CookTime { get; set; } = 0;
+}
