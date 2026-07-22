d73900dcde654bed65405048012c37999c47c429
Shawn Renner <shawn.renner@gmail.com>
2026-07-22 17:08:52 -0500

    comment

--- sloppr/Mappers/MapperDefaults.cs
+++ sloppr/Mappers/MapperDefaults.cs
@@ -1,3 +1,4 @@
 using Riok.Mapperly.Abstractions;
 
+// Do not warn user if DTO does not contain each field from the corresponding model.
 [assembly: MapperDefaults(RequiredMappingStrategy = RequiredMappingStrategy.Target)]
