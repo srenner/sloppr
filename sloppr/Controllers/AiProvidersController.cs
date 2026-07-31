ea753897023080d9ac182c3e17c220415736c629
Shawn Renner <shawn.renner@gmail.com>
2026-07-30 20:44:04 -0500

    controller stubs

---
+++ sloppr/Controllers/AiProvidersController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sloppr.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AiProvidersController : ControllerBase
+    {
+    }
+}
